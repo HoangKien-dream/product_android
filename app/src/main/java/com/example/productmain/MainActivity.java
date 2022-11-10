4083d4f8f3c50371204ca04bedceff96a4d5b06f
HoangKien <hoangkien@gmail.com>
2022-11-10 20:19:33 +0700

    Save code before test.

--- app/src/main/java/com/example/productmain/MainActivity.java
+++ app/src/main/java/com/example/productmain/MainActivity.java
@@ -8,10 +8,16 @@ import android.view.Gravity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private List<Product> productList = new ArrayList<>();
 
     private EditText username;
     private EditText price;
@@ -25,6 +31,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         price  = findViewById(R.id.price);
         save = findViewById(R.id.save);
         save.setOnClickListener(this);
+        initData();
     }
 
     @Override
@@ -36,11 +43,26 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                toast.setGravity(Gravity.TOP|Gravity.CENTER_HORIZONTAL,0,0);
                toast.show();
            }else {
-               Intent intent = new Intent(this,ListProductActivity.class);
-               intent.putExtra("username",username.getText().toString());
-               intent.putExtra("price",price.getText().toString());
-               startActivity(intent);
+                String name = username.getText().toString();
+                int intPrice = Integer.parseInt(price.getText().toString());
+                productList.add(new Product(name,intPrice));
+               ProductAdapter productAdapter = new ProductAdapter(this,productList);
+               ListView listView  = findViewById(R.id.lisProduct);
+               listView.setAdapter(productAdapter);
+               // Show thông báo thành công
+               Toast.makeText(this, "Lưu thành công sản phẩm" + username,Toast.LENGTH_LONG).show();
+//               Intent intent = new Intent(this,ListProductActivity.class);
+//               intent.putExtra("username",username.getText().toString());
+//               intent.putExtra("price",price.getText().toString());
+//               startActivity(intent);
            }
        }
+    }
+
+    private void initData() {
+        productList.add(new Product( "Asus", 1000000));
+        productList.add(new Product( "Dell", 2000000));
+        productList.add(new Product( "Lenovo", 3000000));
+        productList.add(new Product( "MSI", 4000000));
     }
 }
